98cd5d813040c9abdd7a61abd44569a680da4caf
mariannaaspuria <aspuria.mp@gmail.com>
2025-10-29 12:22:30 -0500

    field membes, constructor, getters and setters, derived getter

--- src/main/java/com/pluralsight/Asset.java
+++ src/main/java/com/pluralsight/Asset.java
@@ -1,4 +1,45 @@
 package com.pluralsight;
 
 public class Asset {
+    //field members
+    private String description;
+    private String dateAquired;
+    private double originalCost;
+
+    //constructor
+    public Asset(String description, String dateAquired, double originalCost) {
+        this.description = description;
+        this.dateAquired = dateAquired;
+        this.originalCost = originalCost;
+    }
+
+    //getters and setters
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDateAquired() {
+        return dateAquired;
+    }
+
+    public void setDateAquired(String dateAquired) {
+        this.dateAquired = dateAquired;
+    }
+
+    public double getOriginalCost() {
+        return originalCost;
+    }
+
+    public void setOriginalCost(double originalCost) {
+        this.originalCost = originalCost;
+    }
+
+    //derived getter
+    public double getValue() {
+        return originalCost;
+    }
 }
